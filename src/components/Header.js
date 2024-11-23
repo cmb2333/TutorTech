e2a96dfbb811aebe417cb5f57eec2e3c3914f208
rnf45 <rnf45@nau.edu>
2024-11-22 22:26:38 -0700

    Added Bootstrap to packages and modified the Navbar in Header.js and styles.css.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
 function Header() {
   return (
-    <header>
-      <nav>
-        <div className="logo">
-          <img src="./assets/TutorTech.png" alt="TutorTech Logo" className="logo-img" />
-          <h1>TutorTech</h1>
-        </div>
-        <div className="header-buttons">
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/about" className="nav-link">About</Link>
-          <Link to="/courses" className="nav-link">Courses</Link>
-          <Link to="/signup" className="nav-link">Sign-up</Link>
-          <Link to="/login" className="nav-link">Login</Link>
-        </div>
-      </nav>
-    </header>
+    <Navbar expand="lg" variant='dark' className='navbar-custom'>
+      <Container fluid>
+        <Navbar.Brand className='me-auto'>
+          <img
+            alt="NAU Logo"
+            src="/assets/nau_logo.png"
+            className='logo'
+          />{' '}
+          Metrology Research and Teaching Laboratory
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-center">
+          <Nav className="d-flex mx-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/courses">Courses</Nav.Link>
+            <Nav.Link as={Link} to="about">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/chat">Chat</Nav.Link>
+          </Nav>
+
+        <Nav className='ms-auto'>
+          <Nav.Link as={Link} to="/login">
+            <i className="bi bi-person"></i> Login
+          </Nav.Link>
+
+          <Nav.Link as={Link} to="/signup">
+            <i className="bi bi-door-open"></i> Signup
+          </Nav.Link>
+        </Nav>
+
+        </Navbar.Collapse>
+
+      </Container>
+
+    </Navbar>
   );
 }
 
