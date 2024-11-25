0716a4b5e387667b92ba69db029151b54de262e4
rnf45 <rnf45@nau.edu>
2024-11-25 16:11:27 -0700

    Save Homepage changes

---
+++ src/components/HomeCarousel.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Container, Carousel, Card } from 'react-bootstrap';
+
+
+function HomeCarousel() {
+
+  return (
+    <section className="home-carousel">
+    <Container>
+      <Carousel variant='dark'>
+
+        <Carousel.Item className='carousel-content'>
+          <Card className="carousel-card">
+            <Card.Img variant="top" src="/assets/exampleCourse.jpg" alt="Course Image" />
+            <Card.Body>
+                <Card.Title className="course-title">Course Title 1</Card.Title>
+            </Card.Body>
+            <Card.Body>
+                <Card.Text>A brief description of the course.</Card.Text>
+                <div>
+                <strong>Progress</strong>
+                </div>
+            </Card.Body>
+          </Card>
+        </Carousel.Item>
+
+        <Carousel.Item className='carousel-content'>
+          <Card className="carousel-card">
+            <Card.Img variant="top" src="/assets/exampleCourse.jpg" alt="Course Image" />
+            <Card.Body>
+                <Card.Title className="course-title">Course Title 1</Card.Title>
+            </Card.Body>
+            <Card.Body>
+                <Card.Text>A brief description of the course.</Card.Text>
+                <div>
+                <strong>Progress</strong>
+                </div>
+            </Card.Body>
+          </Card>
+        </Carousel.Item>
+
+        <Carousel.Item className='carousel-content'>
+          <Card className="carousel-card">
+            <Card.Img variant="top" src="/assets/exampleCourse.jpg" alt="Course Image" />
+            <Card.Body>
+                <Card.Title className="course-title">Course Title 1</Card.Title>
+            </Card.Body>
+            <Card.Body>
+                <Card.Text>A brief description of the course.</Card.Text>
+                <div>
+                <strong>Progress</strong>
+                </div>
+            </Card.Body>
+          </Card>
+        </Carousel.Item>
+
+      </Carousel>
+    </Container>
+  </section>
+  );
+}
+
+export default HomeCarousel;
