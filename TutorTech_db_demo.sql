17b4949c2873735bbfe16f7d460dde35e2cf7864
slk325 <148670597+slk325@users.noreply.github.com>
2025-02-04 18:41:45 -0700

    Updated database upload
    - creation of table for course lectures & assignments
    - merge of student_info table and login_info table
    - addition of 3 mock courses for course_information
    - creation of mock data for course_lecture and course_assignment

--- TutorTech_db_demo.sql
+++ TutorTech_db_demo.sql
@@ -6,10 +6,42 @@ CREATE TABLE IF NOT EXISTS course_information
 	course_code VARCHAR(10) NOT NULL PRIMARY KEY,
 	-- course title ex. Metrology
 	course_title VARCHAR(100) NOT NULL,
-	-- description of course
-	course_description TEXT NULL,
 	-- total credits of course
 	credits INT NOT NULL
+);
+
+-- Table: course_lectures
+-- DROP TABLE IF EXISTS course_lectures;
+CREATE TABLE IF NOT EXISTS course_lectures
+(
+	-- lecture id
+	lecture_id VARCHAR(10) NOT NULL PRIMARY KEY,
+	-- course_code
+	course_code VARCHAR(10) NOT NULL,
+	-- lecture title
+	lecture_title VARCHAR(100) NOT NULL,
+	-- video link of lectures
+	video_link VARCHAR(100) NOT NULL,
+	-- foreign key constraint
+	CONSTRAINT course_code FOREIGN KEY (course_code)
+		REFERENCES course_information (course_code)
+);
+
+-- Table: course_assignments
+-- DROP TABLE IF EXISTS course_assignments;
+CREATE TABLE IF NOT EXISTS course_assignments
+(
+    -- assignment id
+	assignment_id VARCHAR(10) NOT NULL PRIMARY KEY,
+	-- course code
+	course_code VARCHAR(10) NOT NULL,
+	-- assignment title
+	assignment_title VARCHAR(100) NOT NULL,
+	-- max score
+	max_score INT NOT NULL,
+	-- foreign key constraint
+	CONSTRAINT course_code FOREIGN KEY (course_code)
+		REFERENCES course_information (course_code)
 );
 
 -- Table: student_information
@@ -23,38 +55,83 @@ CREATE TABLE IF NOT EXISTS student_information
 	last_name VARCHAR(100) NOT NULL,
 	-- user email
 	email VARCHAR(255) NOT NULL
-);
-
--- Table: login_information
--- DROP TABLE IF EXISTS login_information
-CREATE TABLE IF NOT EXISTS login_information
-(
-	-- user identification
-	user_id VARCHAR(8) NOT NULL PRIMARY KEY,
 	-- user password
 	password VARCHAR(20) NOT NULL,
-	-- foreign key constraint
-	CONSTRAINT user_id FOREIGN KEY (user_id)
-		REFERENCES student_information (user_id)
 );
 
--- course_information MOCK DATA inserts
-INSERT INTO course_information (course_code, course_title, course_description, credits)
-	VALUES ('EE499', 'Contemporary Developments: Microelectronics Metrology', 'This course offers a comprehensive study of metrology, focusing on precise measurement and its vital role across industries. Students will explore measurement systems, calibration techniques, uncertainty, and traceability to ensure accuracy and consistency. The course covers error analysis, proper use of metrology tools, and statistical methods for evaluating data. Students will gain hands-on experience with industry-standard equipment, applying metrology principles to real-world scenarios.', 3);
+-- course_information MOCK DATA inserts ----------------------------------------------------------------------
+INSERT INTO course_information (course_code, course_title, credits)
+	VALUES ('EE499', 'Contemporary Developments: Microelectronics Metrology', 3);
 
-INSERT INTO course_information (course_code, course_title, course_description, credits)
-	VALUES ('EE599', 'Device Physics and Characterization', 'This course provides an overview of the fundamental physics governing the electrical, optical, and thermal properties of semiconductor devices. Students will also examine the role of semiconductor materials in modern technology and their applications in electronic and photonic devices. This course is intended for graduate students in electrical engineering, materials science, and physics programs. Prerequisites include courses in semiconductor physics and electrical measurements at the undergraduate level.', 3);
+INSERT INTO course_information (course_code, course_title, credits)
+	VALUES ('EE599', 'Device Physics and Characterization', 3);
 
-INSERT INTO course_information (course_code, course_title, course_description, credits)
-	VALUES ('PHY530', 'Spectroscopy', 'This graduate-level course provides an in-depth overview of spectroscopic techniques used to probe the electronic, vibrational, rotational, and magnetic structure of atoms, molecules, and solids. Theoretical foundations of atomic and molecular spectroscopy are developed, including treatment of absorption, emission, scattering of electromagnetic radiation.', 3);
+INSERT INTO course_information (course_code, course_title, credits)
+	VALUES ('PHY530', 'Spectroscopy', 3);
 
---student_information MOCK DATA insert
-INSERT INTO student_information (user_id, first_name, last_name, email)
-	VALUES ('jh1', 'Josefina', 'Hoffman', 'jh1@gmail.com'), ('tm2', 'Titus', 'Munoz', 'tm2@gmail.com'),('db3', 'Darnell', 'Brady', 'jdb3@gmail.com');
+INSERT INTO course_information (course_code, course_title, credits)
+	VALUES ('DOE', 'Design of Experiments', 3);
 
--- login_information MOCK DATA insert
-INSERT INTO login_information
-	VALUES ('jh1', 'jhPassword'), ('tm2', 'tmPassword'), ('db3', 'dbPassword');
+INSERT INTO course_information (course_code, course_title, credits)
+	VALUES ('EGRFE', 'Fundamental Engineering', 3);
 
+INSERT INTO course_information (course_code, course_title, credits)
+	VALUES ('SPC', ' Statistical Process Control', 3);
+
+-------------------------------------------------------------------------------------------------------------
+
+-- course_lectures MOCK DATA inserts ------------------------------------------------------------------------
+INSERT INTO course_lectures(lecture_id, course_code, lecture_title, video_link)
+	VALUES ('LEC01', 'EE499', 'EE499 - Lecture', 'https://youtu.be/1mqXu3eXylI');
+
+INSERT INTO course_lectures(lecture_id, course_code, lecture_title, video_link)
+	VALUES ('LEC02', 'EE599', 'EE599 - Lecture', 'https://youtu.be/0N9fVpQmDCQ');
+
+INSERT INTO course_lectures(lecture_id, course_code, lecture_title, video_link)
+	VALUES ('LEC03', 'PHY530', 'PHY530 - Lecture', 'https://youtu.be/ifxaJ3lZ-yc');
+
+INSERT INTO course_lectures(lecture_id, course_code, lecture_title, video_link)
+	VALUES ('LEC04', 'DOE', 'DOE - Lecture', 'https://youtu.be/2bKGmQCDBtQ');
+
+INSERT INTO course_lectures(lecture_id, course_code, lecture_title, video_link)
+	VALUES ('LEC05', 'EGRFE', 'EGRFE - Lecture', 'https://youtu.be/azFrptUJOlI');
+
+INSERT INTO course_lectures(lecture_id, course_code, lecture_title, video_link)
+	VALUES ('LEC06', 'SPC', 'SPC - Lecture', 'https://youtu.be/B9h5_eVaGXM');
+
+-------------------------------------------------------------------------------------------------------------
+
+-- course_assignments MOCK DATA inserts ------------------------------------------------------------------------
+INSERT INTO course_assignments(assignment_id, course_code, assignment_title, max_score)
+	VALUES ('ASGMT01', 'EE499', 'EE499 - Assignment 1', '5');
+
+INSERT INTO course_assignments(assignment_id, course_code, assignment_title, max_score)
+	VALUES ('ASGMT02', 'EE599', 'EE599 - Assignment 1', '10');
+
+INSERT INTO course_assignments(assignment_id, course_code, assignment_title, max_score)
+	VALUES ('ASGMT03', 'PHY530', 'PHY530 - Assignment 1', '15');
+
+INSERT INTO course_assignments(assignment_id, course_code, assignment_title, max_score)
+	VALUES ('ASGMT04', 'DOE', 'DOE - Assignment 1', '20');
+
+INSERT INTO course_assignments(assignment_id, course_code, assignment_title, max_score)
+	VALUES ('ASGMT05', 'EGRFE', 'EGRFE - Assignment 1', '25');
+
+INSERT INTO course_assignments(assignment_id, course_code, assignment_title, max_score)
+	VALUES ('ASGMT06', 'SPC', 'SPC - Assignment 1', '30');
+	
+-------------------------------------------------------------------------------------------------------------
+
+-- student_information MOCK DATA insert
+INSERT INTO student_information (user_id, first_name, last_name, email, password)
+	VALUES ('jh1', 'Josefina', 'Hoffman', 'jh1@gmail.com', 'jhPassword');
+
+INSERT INTO student_information (user_id, first_name, last_name, email, password) 
+	VALUES ('tm2', 'Titus', 'Munoz', 'tm2@gmail.com', 'tmPassword');
+
+INSERT INTO student_information (user_id, first_name, last_name, email, password) 
+	VALUES ('db3', 'Darnell', 'Brady', 'jdb3@gmail.com', 'dbPassword');
+
+-------------------------------------------------------------------------------------------------------------
 
 
